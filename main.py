f01e01aa5d86f77a93b1bfd305e428b2ee929576
anyaradchuk87 <anyaradchuk@gmail.com>
2022-09-20 21:42:22 +0100

    wikibot

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
 import telebot
-from telebot import types
+import wikipedia
+import re
 
 bot = telebot.TeleBot('5535732977:AAGBPfXThCvnWoQNOkq5uMKtbaABuMSR85Y')
 
+wikipedia.set_lang('en')
+
+
+def get_wiki(word):
+    try:
+        ny = wikipedia.page(word)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if len(x.strip()) > 3:
+                    wikitext2 = wikitext2 + x + '.'
+            else:
+                break
+
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
+
+        return wikitext2
+
+    except Exception as e:
+        return 'There is no information in wiki about this'
+
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    mess = f'Hello {message.from_user.first_name} {message.from_user.last_name}'
-    bot.send_message(message.chat.id, mess, parse_mode='html')
-
-
-@bot.message_handler(content_types=['photo'])
-def get_user_photo(message):
-    bot.send_message(message.chat.id, 'Wow, cool photo!')
-
-
-@bot.message_handler(commands=['website'])
-def website(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Wikipedia', url='https://wikipedia.ua'))
-    bot.send_message(message.chat.id, 'Visited site', reply_markup=markup)
-
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    website = types.KeyboardButton('Website')
-    start = types.KeyboardButton('Start')
-    markup.add(website, start)
-    bot.send_message(message.chat.id, reply_markup=markup)
+def start(message, res=False):
+    bot.send_message(message.chat.id, 'Send me word and I\'ll find information')
 
 
 @bot.message_handler(content_types=['text'])
-def get_user_text(message):
-    if message.text == 'hello':
-        bot.send_message(message.chat.id, 'Hi there', parse_mode='html')
-    elif message.text == 'id':
-        bot.send_message(message.chat.id, f'Your ID is {message.from_user.id}', parse_mode='html')
-    elif message.text == 'photo':
-        photo = open('logo.png', 'rb')
-        bot.send_photo(message.chat.id, photo)
-    else:
-        bot.send_message(message.chat.id, 'I don\'t understand you', parse_mode='html')
+def get_user_photo(message):
+    bot.send_message(message.chat.id, get_wiki(message.text))
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)
